ac57f19a8fbfdedf284cb116caa8761831bcd422
faheemfazal <faheemfazalpkv@gmail.com>
2024-07-26 18:19:04 +0530

    skeliton loader

--- src/components/modal/Toggle.js
+++ src/components/modal/Toggle.js
@@ -9,14 +9,16 @@ const ToggleSwitch = () => {
 
     return (
         <div className="flex  ">
-          Shop Closed <div 
-                className={`relative w-16 h-8 flex items-center rounded-full p-1 cursor-pointer ${isOn ? 'bg-green-500' : 'bg-gray-300'}`}
+          <h1 className={`font-bold  ${isOn ? 'bg-green-500' : 'bg-red-500'}`}>{isOn?'OPEN':'CLOSED'}</h1>
+          <div 
+                className={`relative w-28 h-12 flex items-center rounded-full p-1 cursor-pointer ${isOn ? 'bg-green-500' : 'bg-red-500'}`}
                 onClick={handleToggle}
             >
                 <div 
                     className={`bg-white w-6 h-6 rounded-full shadow-md transform ${isOn ? 'translate-x-8' : 'translate-x-0'} transition-transform duration-300`}
                 ></div>
-            </div>Shop open
+            </div>
+            
         </div>
     );
 };
